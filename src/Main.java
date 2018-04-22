86cb04717ba9ee66d9435ed8b41a1b3af31c25fd
jsiilva <jsiilva@outlook.com.br>
2018-04-22 01:01:35 -0300

    Commit inicial. Desenvolvendo o arcabouço da estrutura principal da agenda, e definindo o to-do dos próximos dois dias subsequentes.

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import com.br.ivaniciojr.agenda.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Estrutura de dados da agenda
+        Atributos atributos = new Atributos();
+
+        /*
+        *
+        * Aqui eu exibo o menu inicial com as opções de: agendar, cancelar e verificar um compromisso;
+
+        * Uma vez selecionado a opção agendamento, o sistema exibe as entradas de texto para inserir um nome, a descrição, a data e o horário de inicio e fim;
+
+        * Em cancelar, ele exibe os compromissos já agendados com opções correspondentes para remover cada um;
+
+        * Ver os compromissos do dia
+
+        * Verificar choque entre horarios do compromisso. Como meu horário está definido na estrutura como um vetor, posso utilizar o mesmo recurso no algoritmo do balanceamento de chaves para verificar item por item. Pode não ser uma solução inovadora e eficiente por ser em escala linear de processamento, mas acredito que possa funcionar.
+
+        * */
+    }
+
+    private static void exibeData (){
+
+        Date data = new Date();
+        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println(f.format(data));
+    }
+}
