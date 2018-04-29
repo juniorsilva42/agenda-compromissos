e7d9efe30bfad0919b63642d6f6a88bc3a376301
jsiilva <jsiilva@outlook.com.br>
2018-04-29 20:47:39 -0300

    Função de validação de hora devidamente finalizada e pronta para uso no sistema.

--- src/com/br/ivaniciojr/agenda/TestaRecursos.java
+++ src/com/br/ivaniciojr/agenda/TestaRecursos.java
@@ -7,7 +7,7 @@ public class TestaRecursos {
 
     public static void main(String[] args) {
 
-        String horario = "08:30:00:100";
+        String horario = "10:59";
 
         System.out.println(validaHora(horario));
     }
@@ -19,52 +19,47 @@ public class TestaRecursos {
         return true;
     }
 
-    public static boolean validaHora (String horario){
+    public static boolean validaHora (String horario) {
 
         String[] arranjo = horario.split(":");
+        int tamanhoArranjo = arranjo.length;
+
         int horas = Integer.parseInt(arranjo[0]);
-        int minutos = Integer.parseInt(arranjo[1]);
+        int minutos = 0;
+
+        if (tamanhoArranjo >= 2) minutos = Integer.parseInt(arranjo[1]);
 
         // Quebro a string em um Array de carateres
         char[] arranjoQuebrado = String.valueOf(horario).toCharArray();
         int contador = 0;
 
-        // Percorro todos os caracteres da string e passo um a um mediante a variável itensHorario
-        // Com isso, posso verificar se o horário é do tipo 00:00, com a ajuda de uma var auxiliar: contador.
-
-        for (int i = 0; i < arranjo.length; i++){
-            String teste = arranjo[i];
-        }
-
+        /*
+         *
+         * Percorro todos os caracteres da string e passo um a um mediante a variável itensHorario, com isso, posso verificar se o horário é do tipo 00:00, com a ajuda de uma var auxiliar: "contador".
+         *
+         *
+        */
         for (char itensHorario: arranjoQuebrado) {
-            if (itensHorario == ':')
+
+            if (itensHorario == ':') {
                 contador++;
+                continue;
+            }
+
+            if (!Character.isDigit(itensHorario))
+                return false;
         }
 
-        if (contador < 1 || contador >= 3){
+        if (contador < 1 || contador >= 3)
             return false;
-        } else if (horas < 0 || horas > 23){
+        else if (horas < 0 || horas > 23)
             return false;
-        } else if (minutos < 0 || minutos > 59){
+        else if (minutos < 0 || minutos > 59)
             return false;
-        }
 
         return true;
     }
 
-    public boolean ehInteiro (String s) {
-        char[] c = s.toCharArray();
-        boolean d = true;
-
-        for (int i = 0; i < c.length; i++) {
-            // verifica se o char não é um dígito
-            if ( !Character.isDigit( c[ i ] ) ) {
-                d = false;
-                break;
-            }
-    }
-    return d;
-}
 
     public static boolean validaHora2 (String horario) {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
